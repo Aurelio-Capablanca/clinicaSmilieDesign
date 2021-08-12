a966a7f8febc3c793fdf40e9027412ad70332abe
arwenKonne <78279066+arwenKonne@users.noreply.github.com>
2021-08-12 10:42:36 -0600

    cambios

--- Smile Design/smileDesign/app/helpers/private/dashboardPage.php
+++ Smile Design/smileDesign/app/helpers/private/dashboardPage.php
@@ -49,9 +49,9 @@ class Dashboard_Page {
                     <li>  
                         <div class="user-view">
                             <div class="background">
-                                <img src="../../resources/img/SmileDesign.jfif"> 
+                                <img src="../../resources/img/fonfo12.jpg"> 
                             </div>
-                            <a href="#user"><img class="circle" src="../../resources/img/default-user-image.png"></a>
+                            <a href="#user"><img class="circle" src="../../resources/img/SmileDesign.jfif"></a>
                             <a href="#name"><span class="white-text name">Usuario:<b> '. $_SESSION['aliasusuario'].'</b></span></a>
                             <br>                            
                             <br>
